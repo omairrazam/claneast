8f5774d38a3d207205dfdaee34954b127ad973f9
Omair <omair@gems.techverx.com>
2016-10-08 18:12:32 +0500

    slideshow + featured/discount design adjustments

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,5 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( style.scss )
 Rails.application.config.assets.precompile += %w( bootstrap-theme.css )
 Rails.application.config.assets.precompile += %w( responsiveslides.min.js )
+Rails.application.config.assets.precompile += %w( owl.carousel.css )
+Rails.application.config.assets.precompile += %w( owl.carousel.js )
