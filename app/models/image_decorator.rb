d3e929eac6566b0ee5e92e7a6b964ce87e1477c5
Omair <omair@gems.techverx.com>
2016-11-08 22:10:34 +0500

    products page fixed.

---
+++ app/models/image_decorator.rb
@@ -0,0 +1,9 @@
+Spree::Image.class_eval do
+  attachment_definitions[:attachment][:styles] = {
+    :mini => '48x48>', # thumbs under image
+    :small => '100x100>', # images on category view
+    :product => '320x300!', # full product image
+    :large => '600x600>', # light box image
+    :featured => '224x166!'
+  }
+end
