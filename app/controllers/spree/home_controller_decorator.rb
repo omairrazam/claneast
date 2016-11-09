33958ddfe8851867321c14d0e82fb2dfe6b3f713
Omair <omair@gems.techverx.com>
2016-11-09 21:55:47 +0500

    same as last

--- app/controllers/spree/home_controller_decorator.rb
+++ app/controllers/spree/home_controller_decorator.rb
@@ -2,16 +2,16 @@ module Spree
   HomeController.class_eval do
   	before_action :discounted_products, only: :index
   	
-  	def careers
-  	end
+  	# def careers
+  	# end
 
-  	def about_us
-  	end
+  	# def about_us
+  	# end
   	
   	private
   	def discounted_products
-  		@discounted_products = Spree::Product.sale_products
-  		@featured_products   = Spree::Product.featured_products
+  		@discounted_products = Product.sale_products
+  		@featured_products   = Product.featured_products
   	end
   end
 end
