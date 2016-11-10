5775ea7c22dbf0a221dae68fb18c244a43c11f14
Omair <omair@gems.techverx.com>
2016-11-10 14:09:47 +0500

    images path changed - testing

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :branch, :d_home_content
 set :deploy_to, '/home/deploy/claneast'
 set :pty, true
 set :linked_files, %w{config/database.yml config/application.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 set :keep_releases, 5
 set :rvm_type, :user
 
