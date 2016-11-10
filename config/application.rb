a647861e92dbed63f105476f150466f7540b02d9
Omair <omair@gems.techverx.com>
2016-11-10 14:29:17 +0500

    path adding.

--- config/application.rb
+++ config/application.rb
@@ -36,6 +36,6 @@ module Spree
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+    config.assets.paths << Rails.root.join("vendor", "assets", "images")
   end
 end
